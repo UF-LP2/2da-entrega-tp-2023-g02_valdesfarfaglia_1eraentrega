cd4aaed962b8da758685c3b2f54b6a571c19d85d
bautistafarfaglia <56890443+bautistafarfaglia@users.noreply.github.com>
2023-11-09 23:57:31 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def main() -> None:
-  print("Hello World")
+from fuente.fuente import simulacion
 
-if __name__ == "__main__":
-  main()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    simulacion()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
